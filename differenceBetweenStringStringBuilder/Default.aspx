19511762713fabc628578d282260bdb2d8c5acba
tB1ack <31832634+tB1ack@users.noreply.github.com>
2018-02-16 23:25:39 -0800

    Add files via upload

---
+++ differenceBetweenStringStringBuilder/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="differenceBetweenStringStringBuilder.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="stringButton" runat="server" OnClick="stringButton_Click" Text="String" />
+            <br />
+            <br />
+            <asp:Button ID="stringBuilderButton" runat="server" OnClick="stringBuilderButton_Click" Text="String Builder" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ differenceBetweenStringStringBuilder/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace differenceBetweenStringStringBuilder
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void stringButton_Click(object sender, EventArgs e)
+        {
+            string result = "This is a simple string.";
+            result = result + " And this is actually a new string!";
+            resultLabel.Text = result;
+        }
+
+        protected void stringBuilderButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder _sb = new StringBuilder();
+            _sb.Append("This ");
+            _sb.Append("is ");
+            _sb.Append("a ");
+            _sb.Append("string ");
+            _sb.Append("made ");
+            _sb.Append("with ");
+            _sb.Append("StringBuilder!");
+            resultLabel.Text = _sb.ToString();
+        }
+
+        
+    }
+}
